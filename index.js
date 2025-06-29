59e40fd5be01b0ebcc78d94b4a5f82187f1e7af9
Beatrice Wambui <bettyneema2@gmail.com>
2025-06-29 12:44:42 +0300

    the dom modyfying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// remove the <main> element with id="main"
+const main = document.getElementById('main'); //get the main element
+if (main) main.remove(); // remove it from the DOM
+
+// create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// set its id to "victory"
+newHeader.id = 'victory';
+
+// set its text to "YOUR-NAME is the champion" using either .textContent or .innerHTML
+newHeader.textContent= 'Beatrice is the champion';
+
+// assign the new <h1> element to a variable named newHeader
+document.body.appendChild(newHeader);
